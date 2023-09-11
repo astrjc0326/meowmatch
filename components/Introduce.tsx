f01ec6d76a5c0d325695945abf2a5a5efb95c43d
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2023-09-10 23:12:34 -0700

    feature: new component Introduce add

---
+++ components/Introduce.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Introduce () {
+  return (
+    <View>
+      <Text>Introduce yourself!</Text>
+    </View>
+  )
+}
