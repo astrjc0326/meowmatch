74f5c16b37d552f4850a3105d7cddacce894d39e
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2023-09-13 23:39:54 -0700

    feature: init comment

---
+++ components/SwipCats.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function SwipCats() {
+  return (
+    <View>
+      <Text>Swip cats page</Text>
+    </View>
+  )
+}
