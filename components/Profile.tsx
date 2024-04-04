76f551247d2e0dda24e67257adcd166ec5d4c440
Anny Wang <astrjc.anny@gmail.com>
2024-04-04 12:24:32 -0700

    refactor: tailwind css

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -2,47 +2,17 @@ import React, {useState, useEffect} from 'react';
 import {View, Text,TouchableOpacity, Image, StyleSheet} from 'react-native';
 import Cat from '../assets/Cat';
 import styles from '../assets/styles';
+import tw from 'twrnc';
 
 export default function Profile() {
   return(
-    <View style={styles.container}>
-    <Cat style={stylesheet.photo}/>
-    <View style={stylesheet.row}>
-    <Text style={stylesheet.name}>Ddangkong</Text>
-    <Text style={stylesheet.age}>3y3m</Text>
+    <View style={tw`flex-1 items-center justify-center bg-theme.colors.primary`}>
+    <Cat style={tw`mt-5 `}/>
+    <View style={tw`flex-1 flex-row content-around justify-around`}>
+    <Text style={tw`text-xl font-bold`}>Ddangkong</Text>
+    <Text style={tw`text-xl ml-3`}>3y3m</Text>
     </View>
     </View>
   )
 
 }
-
-const stylesheet = StyleSheet.create ({
-  photo: {
-    marginTop: 50
-  },
-  row: {
-    flex: 1,
-    flexDirection: 'row',
-    alignContent: 'space-around',
-    justifyContent: 'space-around',
-  },
-  footer: {
-    position: 'absolute',
-    bottom: '2%',
-
-  },
-  name: {
-    fontSize: 30,
-    fontWeight: '500',
-  },
-  age: {
-    fontSize: 30,
-    marginLeft: 5
-  },
-  svg: {
-    margin: 10
-  },
-  paddingtop: {
-    marginTop: 5
-  }
-})
