6a73b80508648d1e3977999d7adfddb533c87674
Anny Wang <astrjc.anny@gmail.com>
2024-04-04 11:58:31 -0700

    refactor: tailwind css

--- components/MobileLogin.tsx
+++ components/MobileLogin.tsx
@@ -1,7 +1,5 @@
-import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
-
-
 
 export default function MobileLogin( {navigation}: any ) {
   return (
@@ -20,85 +18,11 @@ export default function MobileLogin( {navigation}: any ) {
         </View>
       </View>
 
-      <TouchableOpacity style={[tw`absolute top-35 bg-theme.colors.primary rounded-3xl py-3 px-12 mt-3`,{ backgroundColor: '#e9f5f5'}]} onPress={() => navigation.navigate("Introduce")}>
-        <Text style={tw`text-xl font-semibold text-grey-200`}>Continue</Text>
+      <TouchableOpacity style={[tw`absolute top-35 rounded-3xl py-3 px-12 mt-3`,{ backgroundColor: '#e9f5f5'}]} onPress={() => navigation.navigate("Introduce")}>
+        <Text style={tw`text-xl font-semibold text-gray-200`}>Continue</Text>
       </TouchableOpacity>
       </View>
     </View>
   )
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   button: {
-//     postion: "absolute",
-//     top: "-10%",
-//     backgroundColor: "#e9f5f5",
-//     borderRadius: 30,
-//     // marginTop: 30,
-//     // marginHorizontal: 85,
-
-//   },
-//   btnText: {
-//     fontWeight: "600",
-//     borderRadius: 30,
-//     paddingVertical: 15,
-//     paddingHorizontal: 150,
-//   },
-//   phoneviewcontainer: {
-//     // position: "absolute",
-//     // top: "30%",
-//     width: '80%',
-//     height: 40,
-//     fontSize : 20,
-//     fontWeight : 'bold',
-//     borderColor : 'grey',
-//     borderWidth : 4,
-//     borderRadius : 7,
-//   },
-//   numberInput: {
-//     position: "relative",
-//     width: "100%",
-//     height: "100%",
-//     fontSize: 20,
-//     marginLeft: 10
-//   },
-//   title: {
-//     position: "absolute",
-//     top: "8%",
-//     color: "#b0c5c5",
-//     fontSize: 25,
-//     fontWeight: "600"
-//   },
-//   countrycode: {
-//     color: "#6a6767",
-//     fontSize: 20,
-//     marginTop : 9,
-//     marginRight: 9
-//   },
-//   phoneinput: {
-//     flexDirection: 'row',
-//     position: "absolute",
-//     top: "25%",
-
-//   },
-//   description: {
-//     position: "absolute",
-//     top: "15%",
-//     marginHorizontal: 30,
-//     color: "#6a6767"
-//   },
-//   exitbtn: {
-//     position: "absolute",
-//     left: "2%",
-//     top: "3.5%",
-//     margin: 5
-//   },
-//   exittext: {
-//     color: "#6a6767"
-//   }
-// })
