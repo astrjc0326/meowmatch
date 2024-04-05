dda13e173e81ff6cb96525f1a03c63f7a4ab69bd
Anny Wang <astrjc.anny@gmail.com>
2024-04-05 15:22:44 -0700

    style: layout changes

--- components/LogIn.tsx
+++ components/LogIn.tsx
@@ -3,7 +3,8 @@ import { Text, View, TouchableOpacity, TextInput} from 'react-native';
 import LOGOSVG from "../assets/Logo";
 import RightLogo from "../assets/RightLogo";
 import LeftLogo from "../assets/LeftLogo";
-import tw from 'twrnc';
+// import tw from 'twrnc';
+import tw from './lib/tailwind';
 
 
 export default function LogIn ({ navigation }: any) {
@@ -25,21 +26,22 @@ export default function LogIn ({ navigation }: any) {
     navigation.navigate("SignUp")
   }
     return (
-      <View style={tw`flex-1 bg-theme.colors.primary justify-center items-center`}>
-      <RightLogo style={tw`absolute left-0 top-20`} />
-      <LeftLogo style={tw`absolute right-0 top-20`} />
-      <LOGOSVG style={tw`absolute top-10`} />
-      <Text style={[tw`text-5xl font-bold pt-1`, { color: '#b0c5c5'} ]}>MeowMatch</Text>
+      <View style={tw`bg-primary flex-1 justify-center items-center w-full`}>
+      <View style={tw`flex-1 w-full h-2 justify-center items-center`}>
+      <RightLogo style={tw`absolute left-0`} />
+      <LeftLogo style={tw`absolute right-0`} />
+      <LOGOSVG />
+      <Text style={tw`absolute bottom-0 text- center text-5xl font-bold pt-1 text-titlecolor`}>MeowMatch</Text>
+      </View>
+      <View style={tw`items-center w-full flex-3`}>
+        <TextInput style={tw`px-2 mx-30  w-3/4 text-base h-11  text-left border-2 border-gray-200 rounded-lg bg-white  justify-center`}onChangeText={handleUserNameOnChange} placeholder='username'></TextInput>
+        <TextInput style={tw`px-2 mx-30 w-3/4 text-base h-11  text-left border-2 border-gray-200 rounded-lg bg-white m-2 justify-center`} onChangeText={handlePasswordOnChange} placeholder='password' secureTextEntry={true} ></TextInput>
 
-      <View style={tw`items-center`}>
-        <TextInput style={tw` p-3 mx-30  w-1/4 text-lg h-auto`}onChangeText={handleUserNameOnChange} placeholder='username'></TextInput>
-        <TextInput style={tw`p-3 mx-30 w-1/4 text-lg h-auto`} onChangeText={handlePasswordOnChange} placeholder='password' secureTextEntry={true} ></TextInput>
-
-      <TouchableOpacity style={tw`w-3/4 p-35 bg-gray-200 rounded-lg py-3 mb-2 mt-2`} onPress={handleLoginInOnPress}>
-          <Text style={tw`text-center`}>Log In</Text>
+      <TouchableOpacity style={tw`w-4/5 rounded-full py-3 mb-2 bg-themecolor`} onPress={handleLoginInOnPress}>
+          <Text style={tw`font-bold text-center text-gray-600 `}>Log In</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={tw`w-3/4 p-35 bg-gray-200 rounded-lg py-3 mb-2 `} onPress={handleSignUpOnPress}>
-          <Text style={tw`text-center`}>Sign Up</Text>
+        <TouchableOpacity style={tw`w-4/5 bg-gray-500 rounded-full py-3 mb-2`} onPress={handleSignUpOnPress}>
+          <Text style={tw`font-bold text-center text-themecolor `}>Sign Up</Text>
         </TouchableOpacity>
 
       </View>
