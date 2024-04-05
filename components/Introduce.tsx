f1ab5a08233fe4fdfa8c54d93f7d7dc69f599b10
Anny Wang <astrjc.anny@gmail.com>
2024-04-05 12:30:35 -0700

    refactor: tailwind css

--- components/Introduce.tsx
+++ components/Introduce.tsx
@@ -1,76 +1,48 @@
 import React, {useState, useEffect} from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import tw from 'twrnc';
 
 export default function Introduce ({navigation}: any) {
   const [isMonPress, setisMonPress] = useState<Boolean>(false);
   const [isFonPress, setisFonPress] = useState<Boolean>(false);
+  // const optionbtnOnPress = [tw`py-1 px-2 border rounded-md my-2`, {backgroudColor: '#e9f5f5'}]
   let onMpressStyle = isMonPress ? styles.optionbtnOnPress : styles.optionbtn;
   let onFpressSytle = isFonPress ? styles.optionbtnOnPress : styles.optionbtn;
   useEffect(() => {}, [isMonPress])
-  return (
-    <View style={styles.container}>
 
-      <Text style={styles.title}>Introduce yourself!</Text>
-      <Text style={styles.description}>Fill out the rest of your details so meows know a little more about you.</Text>
-      <Text style={styles.identify}>I identify as a ....</Text>
+  return (
+    <View style={tw`flex-1 content-center items-center algin-center w-screen`}>
+
+      <Text style={tw`text-center content-center w-full text-gray-400 text-2xl font-semibold my-10 pt-2`}>Introduce yourself!</Text>
+      <Text style={tw`text-gray-600 my-2 text-base text-center`}>Fill out the rest of your details so meows know a little more about you.</Text>
+      <View style={tw`align-left w-full px-3`}>
+      <Text style={tw`my-2 text-lg text-gray-400`}>I identify as a ....</Text>
       <TouchableOpacity style={onFpressSytle} onPress={() => { setisFonPress(true);
         setisMonPress(false)}}
         >
-        <Text style={styles.optiontxt}>Femeow</Text>
+        <Text style={tw`text-lg text-gray-400 text-bold`}>Femeow</Text>
       </TouchableOpacity>
       <TouchableOpacity style={onMpressStyle} onPress={() => { setisMonPress(true);
       setisFonPress(false)}}
       >
-        <Text style={styles.optiontxt}>Meow</Text>
+        <Text style={tw`text-bold text-gray-400 text-lg`}>Meow</Text>
       </TouchableOpacity>
-    <Text style={styles.identify}>Birthday</Text>
-      <TextInput placeholder='MM/DD/YYYY' keyboardType='numeric' placeholderTextColor='#DEDADA' maxLength={8} style={styles.input}></TextInput>
-    <Text style={styles.identify}>Name</Text>
-    <TextInput placeholder='Add your first name' placeholderTextColor="#DEDADA" maxLength={20} style={styles.input}>
+    <Text style={tw`text-bold text-gray-400 text-lg`}>Birthday</Text>
+      <TextInput placeholder='MM/DD/YYYY' keyboardType='numeric' placeholderTextColor='#DEDADA' maxLength={8} style={tw`h-12 text-lg my-1 py-1 px-2 border border-gray-400 rounded-lg`}></TextInput>
+    <Text style={tw`text-bold text-gray-400 text-lg`}>Name</Text>
+    <TextInput placeholder='Add your first name' placeholderTextColor="#DEDADA" maxLength={20} style={tw`h-12 text-lg my-1 py-1 px-2 border border-gray-400 rounded-lg`}>
     </TextInput>
 
 
     <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate("AddPhoto")}>
       <Text style={styles.btntxt}>Continue</Text>
     </TouchableOpacity>
-
+    </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    alignContent: "space-between",
-    justifyContent: "flex-start",
-    marginHorizontal: 30,
-    marginTop: 30,
-    marginBottom: 70
-  },
-  title: {
-    textAlign: "center",
-    alignContent: "center",
-    width: "100%",
-    color: "#b0c5c5",
-    fontSize: 25,
-    fontWeight: "600",
-    marginVertical: "5%",
-    paddingTop: "2%"
-  },
-  description: {
-    // position: "absolute",
-    // top: "18%",
-    color: "#6a6767",
-    marginVertical: "2%",
-    fontSize: 17,
-    textAlign: "center"
-  },
-  identify: {
-    color: "#6a6767",
-    marginVertical: "2%",
-    fontSize: 15
-  },
   optiontxt: {
     color: "#6a6767",
     fontSize: 20,
