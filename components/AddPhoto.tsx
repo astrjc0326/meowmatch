f46e3ea8811f73c1fc84bd751fa1d2c109148df5
Anny Wang <astrjc.anny@gmail.com>
2024-04-05 11:50:18 -0700

    refactor: tailwind css

--- components/AddPhoto.tsx
+++ components/AddPhoto.tsx
@@ -2,42 +2,23 @@ import React, {useState} from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import styles from '../assets/styles';
 import DefaultProfile from '../assets/DefaultProfilePhoto';
+import tw from 'twrnc';
 
 export default function AddPhoto ({navigation}: any) {
 
   return (
-    <View style={{...styles.container,
-    ...stylesheet.bkground, ...stylesheet.container}}>
+    <View style={[tw`bg-white mx-30 flex-1 items-center align-center bg-inherit`]}>
       <View >
       <DefaultProfile />
       </View>
-      <Text style={styles.description}>Let's build your photo</Text>
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate("ProfileCamera")}>
-        <Text style={styles.btntxt}>Add a photo</Text>
+      <Text style={[tw`my-2 text-base text-center`, {color: '#6a6767'}]}>Let's build your photo</Text>
+      <TouchableOpacity style={[tw`w-4/5 items-center rounded-lg mt-4`, {backgroundColor: '#e9f5f5'}]} onPress={() => navigation.navigate("ProfileCamera")}>
+        <Text style={tw`font-semibold text-center rounded-full my-3 `}>Add a photo</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate("SelfIntroduction")}>
-        <Text style={styles.btntxt}>Skip</Text>
+      <TouchableOpacity style={[tw`items-center rounded-lg mt-4 w-4/5`, {backgroundColor: '#e9f5f5'}]} onPress={() => navigation.navigate("SelfIntroduction")}>
+        <Text style={tw`"w-full font-semibold text-center rounded-full my-3 `}>Skip</Text>
       </TouchableOpacity>
 
     </View>
   )
 }
-
-let stylesheet = StyleSheet.create({
-  container: {
-    marginHorizontal: 10
-  },
-  bkground: {
-    backgroundColor: "white"
-  },
-  // profile: {
-  //   position: "absolute",
-  //   top: "30%"
-  // },
-  logo: {
-    position: "absolute",
-    top: "10%",
-  },
-})
-
-
