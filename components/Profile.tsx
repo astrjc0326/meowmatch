74cb16e2f6448e50c0aba898a0509c483ae7b283
Anny Wang <astrjc.anny@gmail.com>
2024-04-22 13:41:43 -0700

    add: different adjustment

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -8,10 +8,17 @@ import {
   ImageBackground,
 } from "react-native";
 import Setting from "../assets/Setting";
+import MyModal from './MyModal';
 
 import tw from "../lib/tailwind";
 
 export default function Profile() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
   return (
     <View style={tw`flex-1 items-center justify-center bg-primary`}>
       <View style={tw`flex-1`}>
@@ -22,14 +29,14 @@ export default function Profile() {
         />
       </View>
       <View style={tw`items-end bottom-7 bg-transparent px-2`}>
-        <View style={tw`rounded-full bg-primary`}>
+        <TouchableOpacity style={tw`rounded-full bg-primary `}  onPress={toggleModal} >
           <Setting style={tw`h-8 w-8`} />
-        </View>
+        </TouchableOpacity>
         </View>
       </View>
       <View style={tw`flex-3 mt-5 flex-col items-center`}>
       <View style={tw`flex-row items-center content-center`}>
-        <Text style={tw`text-3xl text-bold text-gray-600`}>Ddangkong</Text>
+        <Text style={tw`text-3xl font-bold text-gray-600`}>Ddangkong</Text>
         <Text style={tw`text-xl ml-3 text-gray-500`}>3y3m</Text>
       </View>
       <View style={tw`p-2 mx-2`}>
@@ -37,6 +44,9 @@ export default function Profile() {
           My name is Ddangkong. I really like to bite.
         </Text>
         </View>
+        </View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <MyModal modalVisible={modalVisible} toggleModal={toggleModal} />
         </View>
     </View>
   );
