51f327528c11a732997ae86b5a6eba914cedf27a
Anny Wang <astrjc.anny@gmail.com>
2024-04-29 13:27:18 -0700

    add: inital profilesetting component

---
+++ components/ProfileSettings.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ScrollView, View, Text, TouchableOpacity, Switch } from "react-native";
+import tw from "../lib/tailwind";
+
+
+function ProfileSettings() {
+  return (
+    <View style={tw`flex-1 bg-primary`}>
+      <Text>This is setting</Text>
+    </View>
+  )
+}
+
+export default ProfileSettings
